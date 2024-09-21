b58ef28e1ef5c3986d0de324d797332a9c39f580
Jade Miller <mil19017@byui.edu>
2024-09-20 23:45:20 -0600

    01 Assignment: Course Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,13 @@
+var currentDate = new Date();
+var year = currentDate.getFullYear();
+var month = currentDate.getMonth() + 1; 
+var day = currentDate.getDate();
+var hours = currentDate.getHours();
+var minutes = currentDate.getMinutes();
+var seconds = currentDate.getSeconds();
+
+var currentDate = "Last Modification: " + month+ "/" + day + "/" + year + " " + hours + ":" + minutes + ":" + seconds;
+document.getElementById("lastModified").innerHTML = currentDate;
+
+var currentDate = "©" + year;
+document.getElementById("copyRight").innerHTML = currentDate;
